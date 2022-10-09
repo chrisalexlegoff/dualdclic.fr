3159685b4bd6c6071dd76839b57d00017592a393
unknown <contact@christophe-le-goff.com>
2022-10-09 14:58:31 +0200

    enable Google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,26 @@ import React from "react"
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {" "}
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body className="">
         <Main />
         <NextScript />
