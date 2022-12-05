80b622d73fc1dbb9b2d6810f8136f2bd1be8f27a
unknown <contact@christophe-le-goff.com>
2022-12-05 19:23:39 +0100

    optimize image

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 const nextConfig = {
-  reactStrictMode: true,
-}
-
-const withOptimizedImages = require("next-optimized-images")
-const toggleMaintenance = false
-
-module.exports = {
   async redirects() {
     return [
       toggleMaintenance
@@ -23,11 +16,9 @@ module.exports = {
   },
   images: {
     domains: [`${process.env.NEXT_PUBLIC_IMAGES_DOMAIN}`],
-    // minimumCacheTTL: 60,
-    // dangerouslyAllowSVG: true,
-    // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  withOptimizedImages: withOptimizedImages({
-    handleImages: ["svg"],
-  }),
+  reactStrictMode: true,
 }
+const toggleMaintenance = false
+
+module.exports = nextConfig
